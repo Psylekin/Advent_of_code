eaf4a46ef330b2569266e3727d65f6d65df391bc
Ben Lenk-Ostendorf <lebe63@ads.medisinn.net>
2019-06-28 12:23:11 +0200

    Added project 4

--- advent_of_code/4/test_project.py
+++ advent_of_code/4/test_project.py
@@ -1,14 +1,13 @@
 import unittest
-from project1 import pysolve1
+from project import pysolve
 
 class TestFunctions(unittest.TestCase):
     def test_connection_to_project(self):
         """
         Test if connection is available.
         """
-        result = pysolve1.connection_test()
+        result = pysolve.connection_test()
         self.assertAlmostEqual(result, 1)
-
 
 if __name__ == '__main__':
     unittest.main()
--- advent_of_code/QuickSetup/test_project.py
+++ advent_of_code/QuickSetup/test_project.py
@@ -1,14 +1,13 @@
 import unittest
-from project2 import pysolve2
+from project import pysolve
 
 class TestFunctions(unittest.TestCase):
     def test_connection_to_project(self):
         """
         Test if connection is available.
         """
-        result = pysolve2.connection_test()
+        result = pysolve.connection_test()
         self.assertAlmostEqual(result, 1)
-
 
 if __name__ == '__main__':
     unittest.main()
