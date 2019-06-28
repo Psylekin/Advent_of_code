d64d3a1beeb70aba5c84990273942e1637c163b4
Ben Lenk-Ostendorf <lebe63@ads.medisinn.net>
2019-06-28 08:57:05 +0200

    Started project 3

--- advent_of_code/3/project1/pysolve1.py
+++ advent_of_code/3/project1/pysolve1.py
@@ -1,2 +1,26 @@
+import numpy as np
+import pandas as pd
+import re
+
+# claims are structured like this: 
+# Nr.,Startpoint horizont, startpoint vertical, length horizontal, length vertical
+
 def connection_test():
     return 1
+
+def overlap_counter(argument):
+    counter = 0
+    return counter
+
+def clear_claim(dirtyClaim):
+    editedClaim = dirtyClaim.replace(" ", "").lstrip("#")
+    cleanClaim = re.sub(r"\D", ",", editedClaim)
+    return cleanClaim
+
+claimList = [line.rstrip('\n') for line in open('project1/input.txt')]
+fabric = pd.DataFrame(int(0), index=np.arange(1, 1000), columns=np.arange(1000))
+
+
+# Take a claim, add 1 to every number he claims
+# Count for fields where the claimcounter is > 1
+
--- advent_of_code/3/test_project1.py
+++ advent_of_code/3/test_project1.py
@@ -9,6 +9,21 @@ class TestFunctions(unittest.TestCase):
         result = pysolve1.connection_test()
         self.assertAlmostEqual(result, 1)
 
+    def test_overlap_counter(self):
+        """
+        Test if the counter is correct for a simple testcase.
+        """
+        claims = ["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"]
+        result = pysolve1.overlap_counter(claims)
+        self.assertEqual(result,4)
+
+    def clear_claim(self):
+        """
+        Test if the claimes are correctly cleared.
+        """
+        claim = "#1 @ 1,3: 4x4"
+        result = pysolve1.clear_claim(claim)
+        self.assertEqual(result,'1,1,3,4,4')
 
 if __name__ == '__main__':
     unittest.main()
