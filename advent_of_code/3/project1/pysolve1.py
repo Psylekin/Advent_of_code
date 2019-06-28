831bc8c65c5bcf91592ba34f90b35a31aa1e6f83
Ben Lenk-Ostendorf <lebe63@ads.medisinn.net>
2019-06-28 12:01:26 +0200

    Solved riddle 3

--- advent_of_code/3/project1/pysolve1.py
+++ advent_of_code/3/project1/pysolve1.py
@@ -1,26 +1,43 @@
 import numpy as np
 import pandas as pd
 import re
+import time
 
 # claims are structured like this: 
-# Nr.,Startpoint horizont, startpoint vertical, length horizontal, length vertical
+# Nr.,Startpoint horizont, startpoint vertical, width , hight
 
 def connection_test():
     return 1
 
-def overlap_counter(argument):
-    counter = 0
-    return counter
-
 def clear_claim(dirtyClaim):
     editedClaim = dirtyClaim.replace(" ", "").lstrip("#")
-    cleanClaim = re.sub(r"\D", ",", editedClaim)
+    cleanClaim = re.sub(r"\D", ",", editedClaim).split(",")
+    for index, value in enumerate(cleanClaim): cleanClaim[index] = int(value)
     return cleanClaim
 
-claimList = [line.rstrip('\n') for line in open('project1/input.txt')]
+def claim_fabric_piece(fabric,clearClaimList):
+    for claim in clearClaimList:
+        for width in range(claim[3]):
+            for hight in range (claim[4]):
+                fabric.iloc[claim[1] + width, claim[2] + hight] += 1
+    return fabric
+
+def count_overlapping_claims(fabric):
+    return fabric[fabric > 1].count().sum()
+
+def find_nonoverlapping_claim(fabric, claims):
+    for claim in clearClaimList:
+        claimFabric = claimedFabric.loc[claim[1] : claim[1] + claim[3], claim[2] : claim[2] + claim[4]]
+        if count_overlapping_claims(claimFabric) == 0:
+            return claim[0]
+
+start = time.time()
+
+claimList = [line.rstrip('\n') for line in open('advent_of_code/3/project1/input.txt')]
+clearClaimList = list(map(lambda x: clear_claim(x) ,claimList))
 fabric = pd.DataFrame(int(0), index=np.arange(1, 1000), columns=np.arange(1000))
+claimedFabric = claim_fabric_piece(fabric, clearClaimList)
 
-
-# Take a claim, add 1 to every number he claims
-# Count for fields where the claimcounter is > 1
-
+print(count_overlapping_claims(claimedFabric))
+print(find_nonoverlapping_claim(fabric, clearClaimList))
+print("{}".format(time.time() - start))
--- advent_of_code/3/test_project1.py
+++ advent_of_code/3/test_project1.py
@@ -1,4 +1,7 @@
 import unittest
+import pandas as pd
+import numpy as np
+
 from project1 import pysolve1
 
 class TestFunctions(unittest.TestCase):
@@ -9,14 +12,6 @@ class TestFunctions(unittest.TestCase):
         result = pysolve1.connection_test()
         self.assertAlmostEqual(result, 1)
 
-    def test_overlap_counter(self):
-        """
-        Test if the counter is correct for a simple testcase.
-        """
-        claims = ["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"]
-        result = pysolve1.overlap_counter(claims)
-        self.assertEqual(result,4)
-
     def clear_claim(self):
         """
         Test if the claimes are correctly cleared.
@@ -24,6 +19,36 @@ class TestFunctions(unittest.TestCase):
         claim = "#1 @ 1,3: 4x4"
         result = pysolve1.clear_claim(claim)
         self.assertEqual(result,'1,1,3,4,4')
+
+    def test_claim_fabric_piece(self):
+        """
+        Test if the claims claim the correct fabric piece
+        """
+        claimList = [[1,1,1,3,3],[2,2,2,2,2]]
+        fabric = pd.DataFrame(int(0), index=np.arange(1, 6), columns=np.arange(6))
+        result = pysolve1.claim_fabric_piece(fabric,claimList)
+        target = [[0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 0, 0], [0, 1, 2, 2, 0, 0], [0, 1, 2, 2, 0, 0], [0, 0, 0, 0, 0, 0]]
+        self.assertEqual(result.values.tolist(), target)
+
+    def test_count_overlapping_claims(self):
+        """
+        Test if overlappings are counted correctly
+        """
+        fabric = pd.DataFrame(int(0), index=np.arange(1, 6), columns=np.arange(6))
+        fabric.loc[1,1] = 2
+        fabric.loc[1,2] = 5
+        result = pysolve1.count_overlapping_claims(fabric)
+        self.assertEqual(result, 2)
+
+    def test_find_nonoverlapping_claim(self):
+        """
+        Test if the finder is correct.
+        """
+        fabric = pd.DataFrame(int(0), index=np.arange(1, 6), columns=np.arange(6))
+        claims = [[1,0,0,2,2],[2,0,0,1,1],[3,4,4,1,1]]
+        fabric = pysolve1.claim_fabric_piece(fabric,claims)
+        result = pysolve1.find_nonoverlapping_claim(fabric, claims)
+        self.assertEqual(result, 3)
 
 if __name__ == '__main__':
     unittest.main()
