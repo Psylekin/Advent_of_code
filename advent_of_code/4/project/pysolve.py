f675ec32291cd0cdeeed48f57bcf73b7115c0974
Ben Lenk-Ostendorf <lebe63@ads.medisinn.net>
2019-07-11 18:28:24 +0200

    Found most sleeping guard

--- advent_of_code/4/project/pysolve.py
+++ advent_of_code/4/project/pysolve.py
@@ -11,10 +11,28 @@ def split_sleep_entry(entry):
 def convert_to_timestamp(entry):
     return datetime.datetime.strptime(entry, '%Y-%m-%d %H:%M')
 
+def create_durationDf(index, guard):
+    start_ind = index
+    end_ind = index + 1
+
+    start = sleepDf.loc[start_ind,"time"]
+    end = sleepDf.loc[end_ind,"time"]
+    delta = calculate_timeDelta(end_ind, start_ind)
+    
+    durationDf = pd.DataFrame([guard,start, end, delta], index = ["guard", "start", "end", "delta"])
+    return durationDf
+
+def calculate_timeDelta(timestamp_later_index, timestamp_earlier_index):
+    return sleepDf.loc[timestamp_later_index,"time"] - sleepDf.loc[timestamp_earlier_index,"time"]
+
+def strip_guard_text(string):
+    return string.lstrip("Guard #").rstrip(" begins shift")
+
+
 chosen_minute = 0
 guardID = 0
-
-
+sameGuard = True
+guardSleepingTimes = pd.DataFrame(columns=["guard", "start", "end", "delta"])
 
 # Lese alle Daten ein + Bereinige Sie
 sleepList = [line.rstrip('\n') for line in open('advent_of_code/4/project/input.txt')]
@@ -24,38 +42,30 @@ for entry in sleepList:
     entry[0] = convert_to_timestamp(entry[0])
 # Sortiere die Daten chronologisch
 sleepDf = pd.DataFrame(sleepList, columns=["time", "action"]).sort_values(by=['time']).reset_index(drop = True)
+
+print("{}\n".format(sleepDf.head(10)))
+
+for index, entry in enumerate(sleepDf.loc[:,"action"]):
+    if entry.startswith("Guard"):
+        guard = strip_guard_text(entry)
+    elif entry.startswith("falls asleep"):
+        durationDf = create_durationDf(index, guard)
+        guardSleepingTimes = guardSleepingTimes.append(durationDf.T, ignore_index = True)
+
+print(guardSleepingTimes)
+
+for guard in guardSleepingTimes.guard.unique():
+    print("Guard: {}, Time: {}".format(guard, guardSleepingTimes.loc[guardSleepingTimes.guard == guard, "delta"].sum()))
+
 # Errechne wie lange die Guards schlafen
-guardSleepingTimes = pd.DataFrame(columns=["ID","SleepingTime"])
+
 #   Wenn Guard steht fange die Berechnung für einen Guard an / oder setze ihn auf die Liste.
-for index, entry in enumerate(sleepDf.action): 
-    if entry.startswith('Guard'):
-        sleeptime = datetime.timedelta()
-        newguard = False
-        iterator = 1
-        guardID = int(entry.lstrip('Guard #').rstrip(' begins shift'))
-        while newguard == False:
-            if index + iterator + 1 > len(sleepDf) - 2:
-                break
-            sleeptime += sleepDf.time[index + iterator + 1] - sleepDf.time[index + iterator]
-            iterator += 2
-            if sleepDf.action[index + iterator].startswith('Guard'):
-                newguard = True
-                new_sleeping_entry = {"SleepingTime": sleeptime, "ID": guardID}
-                guardSleepingTimes = guardSleepingTimes.append(new_sleeping_entry, ignore_index=True)
 
-highscore = [datetime.timedelta(),0]
-for guardID in guardSleepingTimes.ID:
-    sleepingtime = guardSleepingTimes[guardSleepingTimes.ID == guardID].SleepingTime.sum()
-    if sleepingtime > highscore[0]:
-        highscore[0] = sleepingtime
-        highscore[1] = guardID
-print(highscore)
-
-# HIGHSCORE: Wache Nr. 3167
+# HIGHSCORE: Wache Nr. 3041
 
 # Finde heraus, in welcher Minute er am Meisten schläft (Minute)
 
 # Errechne die Lösung
 
-print("Lösung: {}".format(chosen_minute * guardID))
+#print("Lösung: {}".format(chosen_minute * guardID))
 
--- advent_of_code/4/test_project.py
+++ advent_of_code/4/test_project.py
@@ -1,4 +1,5 @@
 import unittest
+import pandas as pd
 from project import pysolve
 
 class TestFunctions(unittest.TestCase):
@@ -10,6 +11,14 @@ class TestFunctions(unittest.TestCase):
         result = pysolve.connection_test()
         self.assertAlmostEqual(result, 1)
 
+    def test_create_durationSeries(self):
+        """
+        Test if the durationSeries is created correct.
+        """
+        #correctSeries = pd.Series([])
+
+        result = pysolve.create_durationSeries(testSleepDf)
+        self.assertEqual(result, correctSeries)
 
 if __name__ == '__main__':
     unittest.main()
