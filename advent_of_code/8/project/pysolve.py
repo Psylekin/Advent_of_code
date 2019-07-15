95c0cfd4abe371ff4fc6636548c6caa900cd7b56
Ben Lenk-Ostendorf <lebe63@ads.medisinn.net>
2019-07-15 18:24:07 +0200

    Learned about recursion

--- advent_of_code/8/project/pysolve.py
+++ advent_of_code/8/project/pysolve.py
@@ -1,3 +1,21 @@
 def connection_test():
     return 1
 
+def loadData(location):
+    with open(location) as input:
+        return input.read().split()
+def parse(it):
+    # read the number of children and number of metadata
+    num_children, num_metadata = next(it), next(it)
+    # recursively parse children nodes
+    children = [parse(it) for _ in range(num_children)]
+    # read the metadata
+    metadata = [next(it) for _ in range(num_metadata)]
+    return (metadata, children)
+
+# Wow, cool solution!
+
+data = loadData("advent_of_code/7/project/input.txt")
+
+root = parse(map(int, data))
+print(root)
