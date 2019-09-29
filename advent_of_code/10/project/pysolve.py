04e7711d6786df0fad0de58cf10afbbfd4352345
Psylekin <bernardon@hotmail.de>
2019-09-29 11:06:32 +0200

    Solved day 10

--- advent_of_code/10/project/pysolve.py
+++ advent_of_code/10/project/pysolve.py
@@ -18,31 +18,13 @@ def load_data_in_df(): #untested
         return df
 
 def add_velocity(dataDf, days): #untested
-    dataDf.X += dataDf.VeloX * days
-    dataDf.Y += dataDf.VeloY * days
-    return dataDf
+    newDf = dataDf
+    newDf.X += newDf.VeloX * days
+    newDf.Y += newDf.VeloY * days
+    return newDf
 
-newDataDf = dataDf = load_data_in_df()
+dataDf = load_data_in_df()
 lowestValue = 222
-
-for day in range(12000):
-    newDataDf = add_velocity(newDataDf, 1)
-    if len(newDataDf.X.unique()) < lowestValue:
-        plt.clf()
-        lowestValue = len(newDataDf.X.unique())
-        lowestDay = day
-        newDataDf.plot.scatter(x="X",y="Y")
-        plt.show()
-
-print("Day: {}, Value: {}".format(lowestDay, lowestValue))
-plt.clf()
-add_velocity(dataDf, lowestDay -1).plot.scatter(x="X",y="Y")
-add_velocity(dataDf, lowestDay +1).plot.scatter(x="X",y="Y")
-add_velocity(dataDf, lowestDay +2).plot.scatter(x="X",y="Y")
-add_velocity(dataDf, lowestDay +3).plot.scatter(x="X",y="Y")
-add_velocity(dataDf, lowestDay +4).plot.scatter(x="X",y="Y")
-add_velocity(dataDf, lowestDay +5).plot.scatter(x="X",y="Y")
-add_velocity(dataDf, lowestDay +6).plot.scatter(x="X",y="Y")
-add_velocity(dataDf, lowestDay +7).plot.scatter(x="X",y="Y")
-
+newDataDf = add_velocity(dataDf, 10521)
+plot = newDataDf.plot.scatter(x="X",y="Y")
 plt.show()
